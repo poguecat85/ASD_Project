9a2c53d650176b97b9263392324f7b953edc5044
Anthony Pogue <mapjr1508@fullsail.edu>
2012-02-29 09:44:15 -0800

    updated js for signup page

--- js/main.js
+++ js/main.js
@@ -6,30 +6,34 @@ var parseSignupForm = function (data) {
 	console.log(data); 
 };
 
-$('#signup').live('click', function storeData(key){
+$('#signup').live('pageinit', function () {
 	var rbform = $('#recordsignup');
 	var	rberrorslink = $('#rberrorslink');
-	// form validation in jqm
-	rbform.validate({
-		invalidHandler: function(form, validator){
-			rberrorslink.click();
-			var html = '';
-			for(var key in validator.submitted){
-				var label = $('label[for^="' + key +'"]').not('[generated]');
-				var legend = label.closest('fieldset').find('.ui-controlgroup-label');
-				var fieldname = legend.length ? legend.text() : label.text();
-				html += '<li>' + fieldname + '</li>';
-			};
-			$("#signuperrors ul").html(html);
-		},
-		submitHandler: function(){
-			var data = rbform.serializeArray();
-			parseSignupForm(data);
-		}
+	var formSave = $('#submit');
+	
+	formSave.on('click', function(){
+		// form validation in jqm
+		rbform.validate({
+			invalidHandler: function(form, validator){
+				rberrorslink.click();
+				var html = '';
+				for(var key in validator.submitted){
+					var label = $('label[for^="' + key +'"]').not('[generated]');
+					var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+					var fieldname = legend.length ? legend.text() : label.text();
+					html += '<li>' + fieldname + '</li>';
+				}
+				$("#signuperrors ul").html(html);
+			},
+			submitHandler: function(){
+				var data = rbform.serializeArray();
+				console.log(data);		
+			}
+		})
 	});
 	
-	var storeData = function (key) {
-		var flname = new Date();
+	var storeData = function (myData) {
+		var flname = ('#flname');
 	    var male = ('#male');
 	    var female = $('#female').val();
 	    var addy = $('#addy').val();
