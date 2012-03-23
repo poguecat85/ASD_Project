4d45ee91869f408c51d086813d273953af17f6a4
Mitchell Pogue <mapjr1508@gmail.com>
2012-03-22 20:30:53 -0700

    couch add, updated js for eclipse push

--- js/save.js
+++ js/save.js
@@ -1,20 +1,31 @@
-$('#submit').live('click', function saveData(id) {
-	var d = new Date();
-	var key = (d.getTime());
-	var flname = $("#flname").val();
-	var addy = $("#addy").val();
-	var user = $("#username").val();
-	var pword = $("#pword").val();
-	var select = $("#select").val();
-	var item = [
-			flname, 
-			addy, 
-			user, 
-			pword, 
-			select
-		];
-	
-	localStorage.setItem(key, item);
-	alert("Design Saved!");
-	$.mobile.changePage($('#account'));
+$('#displayPage').live('page init', function getData() {
+console.log("HI");
+toggleControls("on");
+    var getListdiv = $('#dataPlay')[0];
+    for (var i = 0, j = localStorage.length; i < j; i++) {
+        var key = localStorage.key(i);
+        var value = localStorage.getItem(key);
+        value = value.split(',');
+
+        $('<div>').attr({
+            'class': 'listDiv'
+        }).appendTo('#list');
+        $('<h3>').html(value[0]).appendTo('.listDiv');
+        $('<p>').html('Date Visited: ' + value[1]).appendTo('.listDiv');
+        $('<p>').html('Order: ' + value[2]).appendTo('.listDiv');
+        $('<p>').html('Type: ' + value[3]).appendTo('.listDiv');
+        $('<p>').html('Rating: ' + value[4]).appendTo('.listDiv');
+        $('<p>').html('Favorite: ' + value[5]).appendTo('.listDiv');
+        $('<p>').html('Zip Code: ' + value[6]).appendTo('.listDiv');
+        $('<p>').html('Comments: ' + value[7]).appendTo('.listDiv');
+        $('<p>').html($('<a>').attr({
+            'href': '#',
+            'onclick': 'deleteItem(' + key + ');'
+        }).html('Delete Beans')).appendTo('.listDiv');
+        $('<p>').html($('<a>').attr({
+            'href': '#',
+            'onclick': 'editItem(' + key + ');'
+        }).html('Edit Beans')).appendTo('.listDiv');
+
+    }
 });
